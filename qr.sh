3ac0ccbe1658b0b2105f99b97d23c2b854bdddb8
BxOxSxS <BxOxSxS@protonmail.com>
2020-10-08 22:36:17 +0200

    Fix script when path to file or dir has space

--- qr.sh
+++ qr.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 if [[ $1 == "create" ]]; then
-     if [ ! -f $2 ]; then
+     if [ ! -f "$2" ]; then
           echo File \"$2\" does not exists!
           exit 1
      fi
+     file="$(basename "$2")"
+     cp -f "$2" "${file}_work"
 
-     if [ -d $2_qr ]; then
+     if [ -d "${file}_qr" ]; then
           while true; do
-               read -p "Dir \"$2_qr\" already exist remove it? Y/N " yn
+               read -p "Dir \"${file}_qr\" already exist remove it? Y/N " yn
                case $yn in
-                    [Yy]* ) rm -rf $2_qr; break;;
+                    [Yy]* ) rm -rf "${file}_qr"; break;;
                     [Nn]* ) exit 130;;
                     * ) echo "Please answer Y or N.";;
                esac
           done
      fi
      echo Creating ascii85...
-     mkdir $2_ascii85
-     ascii85 $2 > $2_ascii85/$2
+     mkdir "${file}_ascii85"
+     ascii85 "${file}_work" > "${file}_ascii85"/"$file"
      echo Creating split...
-     cd $2_ascii85
-     split -b 2953 -d $2 $2.
-     rm -rf $2
+     cd "${file}_ascii85"
+     split -b 2953 -d "$file" "$file."
+     rm -rf "$file"
      files=$(ls | wc -l)
      echo Created $files files
      echo Creating QR...
      n=1
-     for a in $2.*
+     for a in "$file".*
      do
           qrencode  -8 -d 10 -m 1 -o $a.png < $a
           rm -rf "$a"
@@ -34,46 +36,49 @@ if [[ $1 == "create" ]]; then
           ((n++))
      done
      cd ..
-     mv $2_ascii85 $2_qr
+     mv "${file}_ascii85" "${file}_qr"
+     rm "${file}_work"
 elif [[ $1 == "convert" ]]; then
-     if [ ! -d $2 ]; then
-          echo Dir \"$2\" does not exists!
+     if [ ! -d $file ]; then
+          echo Dir \"$file\" does not exists!
           exit 1
      fi
-
-     if [ -d $2_cqr ]; then
+     dir="$(basename "$2")"
+     cp -Rf "$2" "${dir}_work"
+     if [ -d "${dir}_cqr" ]; then
           while true; do
-               read -p "Dir \"$2_cqr\" already exist remove it? Y/N " yn
+               read -p "Dir \"${dir}_cqr\" already exist remove it? Y/N " yn
                case $yn in
-                    [Yy]* ) rm -rf $2_cqr; break;;
+                    [Yy]* ) rm -rf "${dir}_cqr"; break;;
                     [Nn]* ) exit 130;;
                     * ) echo "Please answer Y or N.";;
                esac
           done
      fi
      echo Converting QR...
-     mkdir ${2}_cqr
-     cd $2
+     mkdir "${dir}_cqr"
+     cd "${dir}_work"
      files=$(ls *.png | wc -l)
      echo Finded $files files
      n=1
      for a in ${a}*.png
      do
-          zbarimg  -q $a > ../${2}_cqr/${a}.txt
+          zbarimg  -q $a > "../${dir}_cqr/${a}.txt"
           printf "\r$n/$files"
           ((n++))
      done
      echo Connetcing files...
-     cd ../${2}_cqr
-     cat * > ../${2}_ascii85.txt
-     sed -r 's/^QR-Code://' ../${2}_ascii85.txt > ../${2}_sascii85.txt
+     cd "../${dir}_cqr"
+     cat * > "../${dir}_ascii85.txt"
+     sed -r 's/^QR-Code://' "../${dir}_ascii85.txt" > "../${dir}_sascii85.txt"
      cd ..
      echo Decoding ascii85...
-     ascii85 -d ${2}_sascii85.txt > $2_converted
+     ascii85 -d "${dir}_sascii85.txt" > "${dir}_converted"
      echo Removing working dir and files...
-     rm -rf ${2}_cqr
-     rm -rf ${2}_ascii85.txt
-     rm -rf ${2}_sascii85.txt
+     rm -rf "${dir}_cqr"
+     rm -rf "${dir}_ascii85.txt"
+     rm -rf "${dir}_sascii85.txt"
+     rm -rf "${dir}_work"
 
 else
      echo Unkown  parametr
